6285d417fcd8dd28fb28ea8f3bfbf37930cbaab5
刘博 <liubo1@gg.com>
2023-10-18 12:17:37 +0800

    [初始化] 项目工程

---
+++ file_fun.py
@@ -0,0 +1,11 @@
+import os
+import shutil
+
+
+def clear_directory(directory_path="uploads"):
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        elif os.path.isdir(file_path):
+            shutil.rmtree(file_path)
---
+++ main.py
@@ -0,0 +1,70 @@
+from flask import (
+    Flask,
+    render_template,
+    request,
+    redirect,
+    send_from_directory,
+)
+
+import os
+import threading
+from file_fun import clear_directory
+
+app = Flask(__name__)
+
+# 上传文件存储目录
+UPLOAD_FOLDER = "uploads"
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+
+def perform_cleanup():
+    clear_directory()
+    # 每隔一段时间（例如，每小时）执行清理操作
+    timer = threading.Timer(3600, perform_cleanup)
+    timer.start()
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+# 显示上传文件列表的路由
+@app.route("/uploads")
+def show_uploads():
+    filenames = os.listdir(app.config["UPLOAD_FOLDER"])
+    return render_template("uploads.html", filenames=filenames)
+
+
+# 处理文件上传的路由
+@app.route("/upload", methods=["POST"])
+def upload_file():
+    if "file" not in request.files:
+        return redirect(request.url)
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        return "No selected file"
+
+    if file:
+        file.save("uploads/" + file.filename)
+        return render_template("index.html", message="File uploaded successfully!")
+
+
+@app.route("/download/<filename>")
+def download_file(filename):
+    return send_from_directory(
+        app.config["UPLOAD_FOLDER"], filename, as_attachment=True
+    )
+
+
+if __name__ == "__main__":
+    # 确保上传目录存在
+    if not os.path.exists(UPLOAD_FOLDER):
+        os.makedirs(UPLOAD_FOLDER)
+
+    # 启动清理操作的计时器
+    perform_cleanup()
+
+    app.run(host="0.0.0.0", port=5000, debug=True)
